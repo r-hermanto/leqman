931a1f4de1138b901bb9d5f6b168cbcf4e43f054
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2024-01-08 21:30:31 +0700

    Implement api request executor

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/r-hermanto/leqman/internal/leq"
 )
@@ -10,19 +12,31 @@ func main() {
 	collections := leq.GetCollections()
 	fmt.Printf("collections: %v\n", collections)
 
+	body := map[string]interface{}{
+		"body1": "value body",
+		"body2": 123,
+		"body3": map[string]interface{}{
+			"inner1": 123,
+			"inner2": "inner value 2",
+		},
+	}
+
+	bbody, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	config := leq.LeqConfig{
-		URL:    "locahost:8000/home1",
-		Body:   nil,
-		Header: nil,
+		URL:  "localhost:8000/home",
+		Body: bbody,
+		Header: map[string]string{
+			"header1": "value1",
+			"header2": "value2",
+		},
 		Method: "GET",
 	}
 	leq.UpdateRequest("homepage", config)
 
-	collections = leq.GetCollections()
-	fmt.Printf("collections: %v\n", collections)
-
-	leq.DeleteRequest("homepage")
-
-	collections = leq.GetCollections()
-	fmt.Printf("collections: %v\n", collections)
+	api := leq.GetRequest("homepage")
+	api.Execute()
 }
--- internal/leq/leq.go
+++ internal/leq/leq.go
@@ -1,13 +1,61 @@
 package leq
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"slices"
 )
 
 type LeqConfig struct {
-	URL    string          `json:"url"`
-	Method string          `json:"method"`
-	Header json.RawMessage `json:"header"`
-	Body   json.RawMessage `json:"body"`
+	URL    string            `json:"url"`
+	Method string            `json:"method"`
+	Header map[string]string `json:"header"`
+	Body   json.RawMessage   `json:"body"`
 }
 
+func (l *LeqConfig) Execute() {
+	url := l.URL
+	if url[:4] != "http" {
+		url = "http://" + url
+	}
+
+	body := &bytes.Buffer{}
+	if allowBody(l.Method) {
+		body = bytes.NewBuffer(l.Body)
+	}
+
+	req, err := http.NewRequestWithContext(context.TODO(), l.Method, url, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for k, v := range l.Header {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var pretty bytes.Buffer
+	json.Indent(&pretty, b, "", "    ")
+	fmt.Printf("res.Header: %v\n", resp.Header)
+	fmt.Println(pretty.String())
+}
+
+func allowBody(httpMethod string) bool {
+	allowed := []string{http.MethodPost, http.MethodPut, http.MethodPatch}
+	return slices.Contains(allowed, httpMethod)
+}
